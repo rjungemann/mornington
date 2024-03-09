05cc385d150acb966c9e467a46e0a53f3fc158bc
Roger Jungemann <roger@teamsketchy.com>
2024-03-08 21:17:45 -0800

    Heroku

--- api/src/seed.ts
+++ api/src/seed.ts
@@ -10,7 +10,7 @@ async function main() {
   await truncateAllGameData(db)()
 
   // TODO: Make this an option
-  const paths = await glob('../assets/maps/**/*.svg')
+  const paths = await glob('maps/**/*.svg')
   for (let path of paths) {
     await importGameFromSvg(db)(path)
   }
