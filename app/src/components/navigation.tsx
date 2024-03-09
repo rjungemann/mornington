0cf6b8b562b3c406ec2cea979ff3c100b7cf197d
Roger Jungemann <roger@teamsketchy.com>
2024-03-09 00:48:08 -0800

    Fix margin

--- app/src/components/navigation.tsx
+++ app/src/components/navigation.tsx
@@ -7,7 +7,7 @@ export const Navigation = () => {
         <span className="font-semibold text-xl text-sky-500 tracking-tight font-semibold bg-slate-200 text-slate-800 p-4 pt-2 pb-2 inline-block">Mornington</span>
       </div>
       
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0">
         <div className="text-sm font-semibold lg:flex-grow">
           <Link href="/games" className="block mt-4 lg:inline-block lg:mt-0 opacity-60 hover:opacity-100 mr-4">
             Games
