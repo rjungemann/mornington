c0ce21eedfd60459861ebb87bafe016d698fe45a
Roger Jungemann <roger@teamsketchy.com>
2024-03-14 18:25:33 -0700

    Move CDK stuff to separate repo

--- simple/src/middleware/errorResponder.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export function errorResponder(error: any, req: Request, res: Response, next: NextFunction) {
-  const errorCode = error.code ? error.code : 500;
-  const message = error.message ? error.message : "Server error occurred";
-
-  res.status(errorCode).send(message);
-}
--- simple/src/server.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-import {app} from "./app";
-
-dotenv.config();
-const port = parseInt(process.env.PORT || '3001', 10);
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
